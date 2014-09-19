210a48a7a6698b506de73cad87cf3d2c16456a04
Ricky <rs79c@mail.missouri.edu>
2014-09-19 17:02:44 -0500

    hashMap way of SN

--- src/singleNumber/SingleNumber.java
+++ src/singleNumber/SingleNumber.java
@@ -1,10 +1,13 @@
 package singleNumber;
 
+import java.util.HashMap;
+import java.util.Set;
+
 public class SingleNumber {
 	/**
 	 * Use XOR to implement a linear runtime complexity, without using extra
 	 * memory
-	 * 
+	 *
 	 * @param A
 	 * @return
 	 */
@@ -14,5 +17,28 @@ public class SingleNumber {
 			result = result ^ i;
 		}
 		return result;
+	}
+
+	/**
+	 * hashMap
+	 */
+	public int singleNumberHashMap(int[] A) {
+		HashMap<Integer, Integer> hashMap = new HashMap<Integer, Integer>();
+		for (int i : A) {
+			if (hashMap.containsKey(i)) {
+				hashMap.put(i, hashMap.get(i) + 1);
+			} else {
+				hashMap.put(i, 1);
+			}
+		}
+		Set<Integer> keySet = hashMap.keySet();
+		int k=-1;
+		for (Integer k1 : keySet){
+		    if (hashMap.get(k1)==1){
+		    	k = k1;
+		        break;
+		    }
+		}
+		return k;
 	}
 }
