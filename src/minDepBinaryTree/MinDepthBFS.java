56f133722cb45d86bcd3acf48dd409ecb50563aa
Ricky <rs79c@mail.missouri.edu>
2014-09-21 21:20:17 -0500

    revereseInteger

--- src/minDepBinaryTree/MinDepthBFS.java
+++ src/minDepBinaryTree/MinDepthBFS.java
@@ -7,15 +7,6 @@ import java.util.Queue;
  * Definition for binary tree public class TreeNode { int val; TreeNode left;
  * TreeNode right; TreeNode(int x) { val = x; } }
  */
-class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
-
-	TreeNode(int val) {
-		this.val = val;
-	}
-}
 
 public class MinDepthBFS {
 	public int minDepth(TreeNode root) {
