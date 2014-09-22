7a26059b5ba2aab870ff82a6b23e6f722612dc6d
Ricky <rs79c@mail.missouri.edu>
2014-09-21 22:07:31 -0500

    BuySellStock2

--- src/buySellStock/BuySellStock1.java
+++ src/buySellStock/BuySellStock1.java
@@ -1,5 +1,16 @@
 package buySellStock;
 
+/**
+ * Say you have an array for which the ith element is the price of a given stock
+ * on day i.
+ * 
+ * If you were only permitted to complete at most one transaction (ie, buy one
+ * and sell one share of the stock), design an algorithm to find the maximum
+ * profit.
+ * 
+ * @author Ricky
+ *
+ */
 public class BuySellStock1 {
 	public int maxProfit(int[] prices) {
 		int max = 0;
