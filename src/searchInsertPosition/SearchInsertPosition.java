3e8e4a70fc191a9e1f57c053289d49a6b7ff23ec
Ricky <rs79c@mail.missouri.edu>
2014-09-28 00:02:59 -0500

    SearchInsertPosition

---
+++ src/searchInsertPosition/SearchInsertPosition.java
@@ -0,0 +1,15 @@
+package searchInsertPosition;
+
+public class SearchInsertPosition {
+	public int searchInsert(int[] A, int target) {
+		if (A == null) {
+			return 0;
+		}
+		for (int i = 0; i < A.length; i++) {
+			if (target <= A[i]) {
+				return i;
+			}
+		}
+		return A.length;
+	}
+}
