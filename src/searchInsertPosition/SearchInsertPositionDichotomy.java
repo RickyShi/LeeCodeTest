c4a95ea074bfac9ac2d20edf7ef8108cebd71bdf
Ricky <rs79c@mail.missouri.edu>
2014-09-28 00:28:39 -0500

    searchInsertPosition simple loop way

---
+++ src/searchInsertPosition/SearchInsertPositionDichotomy.java
@@ -0,0 +1,35 @@
+package searchInsertPosition;
+
+/**
+ * dichotomy method is much better
+ *
+ * @author Ricky
+ *
+ */
+public class SearchInsertPositionDichotomy {
+	public int searchInsertDichotomy(int[] A, int target) {
+		if (A == null) {
+			return 0;
+		}
+		int left = 0;
+		int right = A.length - 1;
+		while (left < right) {
+			if (A[left] > target) {
+				return left;
+			} else if (A[right] < target) {
+				return right + 1;
+			} else {
+				int middle = (left + right) / 2;
+				if (A[middle] == target) {
+					return middle;
+				} else if (A[middle] > target) {
+					right = middle - 1;
+				} else {
+					left = middle + 1;
+				}
+			}
+		}
+		return 0;
+
+	}
+}
