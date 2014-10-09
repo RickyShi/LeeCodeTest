84f83cf67fe7087c8d9627dddee30aac28e8f49b
Ricky <rs79c@mail.missouri.edu>
2014-10-09 00:07:55 -0500

    Jump2 Greedy Space O(1) added

--- src/jumpGame/JumpGame2Greedy.java
+++ src/jumpGame/JumpGame2Greedy.java
@@ -1,7 +1,12 @@
 package jumpGame;
 
 public class JumpGame2Greedy {
-
+	/**
+	 * Space O(n)
+	 *
+	 * @param A
+	 * @return
+	 */
 	public int jump(int[] A) {
 		// Need to notice Length<=1
 		if (A == null || A.length <= 1) {
@@ -30,6 +35,39 @@ public class JumpGame2Greedy {
 				max = i + A[i];
 			}
 
+		}
+		return -1;
+	}
+
+	/**
+	 * Not using the minSteps[] Space O(1)
+	 *
+	 * Move with the window[start,end]
+	 *
+	 * @param A
+	 * @return
+	 */
+	public int jumpGreedy2(int[] A) {
+		// Need to notice Length<=1
+		if (A == null || A.length <= 1) {
+			return 0;
+		}
+
+		int start = 0;
+		int end = 0;
+		int count = 0;
+		while (start <= end) {
+			count++;
+			int lastEnd = end;
+			for (int i = start; i <= lastEnd; i++) {
+				if (i + A[i] >= A.length - 1) {
+					return count;
+				}
+				if (end < i + A[i]) {
+					end = i + A[i];
+				}
+			}
+			start = lastEnd + 1;
 		}
 		return -1;
 	}
