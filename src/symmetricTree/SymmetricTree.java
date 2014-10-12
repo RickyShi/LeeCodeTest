962097bf25b4fc8d9d57fa7aa791c9a94a4daa4f
Ricky <rs79c@mail.missouri.edu>
2014-10-11 19:57:01 -0500

    Symmetric Tree Recursive

---
+++ src/symmetricTree/SymmetricTree.java
@@ -0,0 +1,30 @@
+package symmetricTree;
+
+/**
+ * Given a binary tree, check whether it is a mirror of itself (ie, symmetric
+ * around its center).
+ *
+ * For example, this binary tree is symmetric:
+ *
+ * 1
+ * / \
+ * 2 2
+ * / \ / \
+ * 3 4 4 3
+ *
+ * @author Ricky
+ *
+ */
+public class SymmetricTree {
+
+}
+
+class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode(int x) {
+		val = x;
+	}
+}
