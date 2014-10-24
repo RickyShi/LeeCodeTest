6386959fb74ec53655b1c6099642d59c2c8a9730
Ricky <rs79c@mail.missouri.edu>
2014-10-23 22:36:30 -0500

    populateNextRightPointersInEachNode1 Recursive way

---
+++ src/populateNextRightPointersInEachNode/populateNextRightPointersInEachNode1Rec.java
@@ -0,0 +1,20 @@
+package populateNextRightPointersInEachNode;
+
+public class populateNextRightPointersInEachNode1Rec {
+	/**
+	 * Recursive way
+	 */
+	public void connect(TreeLinkNode root) {
+		if (root == null || (root.left == null && root.right == null)) {
+			return;
+		}
+
+		root.left.next = root.right;
+		if (root.next != null) {
+			root.right.next = root.next.left;
+		}
+
+		connect(root.left);
+		connect(root.right);
+	}
+}
