fee50add5fad5c4b20c4a9fb78f7ec6018f40807
Ricky <rs79c@mail.missouri.edu>
2014-10-29 18:16:59 -0500

    FlatternBinaryTreeToLinkedList  recursive added

--- src/flatternBinaryTreeToLinkedList/FlatternBinaryTreeToLinkedList.java
+++ src/flatternBinaryTreeToLinkedList/FlatternBinaryTreeToLinkedList.java
@@ -49,8 +49,22 @@ public class FlatternBinaryTreeToLinkedList {
 				TreeNode temp = stack.pop();
 				p.right = temp;
 			}
-
 			p = p.right;
 		}
 	}
+
+		TreeNode lastVisited = null;
+	    public void flattenRec(TreeNode root) {
+			if (root == null) {
+				return;
+			}
+			TreeNode rightSaved = root.right;
+			if (lastVisited != null) {
+				lastVisited.left = null;
+				lastVisited.right = root;
+			}
+			lastVisited = root;
+			flattenRec(root.left);
+			flattenRec(rightSaved);
+	    }
 }
