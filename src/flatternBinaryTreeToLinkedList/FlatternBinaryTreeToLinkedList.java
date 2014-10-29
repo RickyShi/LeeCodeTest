250c7b8606c4fb83249536d987fed13de819b8da
Ricky <rs79c@mail.missouri.edu>
2014-10-29 16:52:04 -0500

    FlatternBinaryTreeToLinkedList iteractive

---
+++ src/flatternBinaryTreeToLinkedList/FlatternBinaryTreeToLinkedList.java
@@ -0,0 +1,56 @@
+package flatternBinaryTreeToLinkedList;
+
+import java.util.Stack;
+
+import binaryTreeTraversal.TreeNode;
+
+/**
+ * Given a binary tree, flatten it to a linked list in-place.
+ *
+ * For example,
+ * Given
+ *
+ * 1
+ * / \
+ * 2 5
+ * / \ \
+ * 3 4 6
+ * The flattened tree should look like:
+ * 1
+ * \
+ * 2
+ * \
+ * 3
+ * \
+ * 4
+ * \
+ * 5
+ * \
+ * 6
+ *
+ * @author Ricky
+ *
+ */
+public class FlatternBinaryTreeToLinkedList {
+	public void flatten(TreeNode root) {
+		Stack<TreeNode> stack = new Stack<TreeNode>();
+		TreeNode p = root;
+
+		while (p != null || !stack.empty()) {
+
+			if (p.right != null) {
+				stack.push(p.right);
+			}
+
+			if (p.left != null) {
+				p.right = p.left;
+				p.left = null;
+			} else if (!stack.empty()) {
+				TreeNode temp = stack.pop();
+				p.right = temp;
+			}
+
+			p = p.right;
+		}
+	}
+}
