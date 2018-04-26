d57a06e93f088186955035d19fe40de107c1411c
ruiqi.shi <rs79c@mail.missouri.edu>
2018-04-26 13:22:01 -0600

    462 quick select way

--- src/repo_2018/minmumMovesToEqualArrayElements2_462/Solution.java
+++ src/repo_2018/minmumMovesToEqualArrayElements2_462/Solution.java
@@ -17,6 +17,69 @@ public class Solution {
             }
             return count;
         }
+
+        /**
+         * find the median using quick select Avg O(n) See Reference 215
+         */
+        public int minMoves2QuickSelect(int[] nums) {
+
+            int sum = 0, median = selectRecursive(nums, nums.length/2+1);
+            for (int num : nums) sum += Math.abs(num - median);
+            return sum;
+        }
+
+        public  int selectRecursive(int[] array, int n) {
+            return recursive(array, 0, array.length - 1, n);
+        }
+
+        // Returns the n-th smallest element of list within left..right inclusive
+        // (i.e. left <= n <= right).
+        // The size of the list is not changing with each recursion.
+        // Thus, n does not need to be updated with each round.
+        private  int recursive(int[] array, int left, int right, int n) {
+            if (left == right) { // If the list contains only one element,
+                return array[left]; // return that element
+            }
+
+            // select a pivotIndex between left and right
+            int pivotIndex = (right-left)/2;
+            pivotIndex = partition(array, left, right, pivotIndex);
+            // The pivot is in its final sorted position
+            if (n == pivotIndex) {
+                return array[n];
+            } else if (n < pivotIndex) {
+                return recursive(array, left, pivotIndex - 1, n);
+            } else {
+                return recursive(array, pivotIndex + 1, right, n);
+            }
+        }
+
+        /**
+         * In quicksort, there is a subprocedure called partition that can, in
+         * linear time, group a list (ranging from indices left to right) into two
+         * parts, those less than a certain element, and those greater than or
+         * equal to the element. Here is pseudocode that performs a partition about
+         * the element list[pivotIndex]
+         */
+        private  int partition(int[] array, int left, int right, int pivotIndex) {
+            int pivotValue = array[pivotIndex];
+            swap(array, pivotIndex, right); // move pivot to end
+            int storeIndex = left;
+            for (int i = left; i < right; i++) {
+                if (array[i] < pivotValue) {
+                    swap(array, storeIndex, i);
+                    storeIndex++;
+                }
+            }
+            swap(array, right, storeIndex); // Move pivot to its final place
+            return storeIndex;
+        }
+
+        private  void swap(int[] array, int a, int b) {
+            int tmp = array[a];
+            array[a] = array[b];
+            array[b] = tmp;
+        }
     }
 
     /** Only make sure the target value locates between the middle two level, so it is similar to median concept, yet it is much faster*/
