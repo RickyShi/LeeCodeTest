e5bbbd20a7ca70e6188a673387f38986660b7119
ruiqi.shi <rs79c@mail.missouri.edu>
2018-04-27 11:25:38 -0600

    isSameTree 100

---
+++ src/repo_2018/sameTree_100/Solution.java
@@ -0,0 +1,14 @@
+package repo_2018.sameTree_100;
+
+import repo_2018.commonStructure.TreeNode;
+
+public class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p==null&&q==null) return true;
+        if(p==null||q==null) return false;//p,q both null case is checked
+        if (p.val==q.val){
+            return isSameTree(p.left,q.left)&&isSameTree(p.right,q.right);
+        }
+        return false;
+    }
+}
