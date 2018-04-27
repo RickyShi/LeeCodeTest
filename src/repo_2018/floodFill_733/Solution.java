00a9539e3baae5973bcf5ce035047866de08908d
ruiqi.shi <rs79c@mail.missouri.edu>
2018-04-27 11:17:47 -0600

    733 BFS added

--- src/repo_2018/floodFill_733/Solution.java
+++ src/repo_2018/floodFill_733/Solution.java
@@ -1,7 +1,11 @@
 package repo_2018.floodFill_733;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Solution {
 
+    /** Recursive DFS Method, and more */
     public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
         int oldColor = image[sr][sc];
         //Check whether the color is the same, otherwise cause stack overflow
@@ -19,4 +23,44 @@ public class Solution {
             if(c<image[0].length-1) dfs(image ,r,c+1,newColor,oldColor);
         }
     }
+    /**Use Queue to do BFS*/
+    public int[][] floodFillBFS(int[][] image, int sr, int sc, int newColor) {
+        if (image == null || image.length == 0 || image[0].length == 0) {
+            return new int[0][0];
+        }
+        if (sr < 0 || sr >= image.length) {
+            return image;
+        }
+        if (sc < 0 || sc >= image[0].length) {
+            return image;
+        }
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{sr, sc});
+        int originalValue = image[sr][sc];
+        if (originalValue == newColor) {
+            return image;
+        }
+        image[sr][sc] = newColor;
+        int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
+            for (int[] d : dirs) {
+                int x = cur[0] + d[0];
+                int y = cur[1] + d[1];
+                if (x < 0 || x >= image.length) {
+                    continue;
+                }
+                if (y < 0 || y >= image[0].length) {
+                    continue;
+                }
+                if (image[x][y] != originalValue) {
+                    continue;
+                }
+                image[x][y] = newColor;
+                queue.offer(new int[]{x, y});
+            }
+        }
+        return image;
+    }
 }
+
