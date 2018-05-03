f9e656bc2d9ddee3ca43fd9f7780f3f3aea7222f
ruiqi.shi <rs79c@mail.missouri.edu>
2018-05-03 09:37:24 -0600

    ListNode Structure

---
+++ src/repo_2018/commonStructure/ListNode.java
@@ -0,0 +1,10 @@
+package repo_2018.commonStructure;
+
+/* Definition for singly-linked list.*/
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+    }
+}
