6bf33921701ab8d698d080c3116dbbd7fa2ce233
ruiqi.shi <rs79c@mail.missouri.edu>
2018-05-09 09:28:26 -0600

    599

--- src/repo_2018/minimumIndexSumOfTwoLists_599/Solution.java
+++ src/repo_2018/minimumIndexSumOfTwoLists_599/Solution.java
@@ -6,20 +6,20 @@ public class Solution {
     public String[] findRestaurant(String[] list1, String[] list2) {
         List<String> rtn = new ArrayList<>();
 
-        Map<String,Integer> map1 = new HashMap<>();
-        Map<String,Integer> count = new HashMap<>();
+        Map<String, Integer> map1 = new HashMap<>();
+        Map<String, Integer> count = new HashMap<>();
 
-        for(int i = 0;i<list1.length;i++) {
-            map1.put(list1[i],i);
+        for (int i = 0; i < list1.length; i++) {
+            map1.put(list1[i], i);
         }
-        for(int i = 0;i<list2.length;i++) {
-            if(map1.containsKey(list2[i])){
-                count.put(list2[i],i+map1.get(list2[i]));
+        for (int i = 0; i < list2.length; i++) {
+            if (map1.containsKey(list2[i])) {
+                count.put(list2[i], i + map1.get(list2[i]));
             }
         }
         int min = Collections.min(count.values());
-        for(String k:count.keySet()){
-            if (count.get(k)==min){
+        for (String k : count.keySet()) {
+            if (count.get(k) == min) {
                 rtn.add(k);
             }
         }
@@ -31,11 +31,14 @@ public class Solution {
         Map<String, Integer> map = new HashMap<>();
         List<String> res = new LinkedList<>();
         int minSum = Integer.MAX_VALUE;
-        for (int i=0;i<list1.length;i++) map.put(list1[i], i);
-        for (int i=0;i<list2.length;i++) {
+        for (int i = 0; i < list1.length; i++) map.put(list1[i], i);
+        for (int i = 0; i < list2.length; i++) {
             Integer j = map.get(list2[i]);
             if (j != null && i + j <= minSum) {//a little bit faster than above method
-                if (i + j < minSum) { res.clear(); minSum = i+j; }
+                if (i + j < minSum) {
+                    res.clear();
+                    minSum = i + j;
+                }
                 res.add(list2[i]);
             }
         }
