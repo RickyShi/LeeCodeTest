08f14ee346fcb9896eb00711d674e7e76e64064f
ruiqi.shi <rs79c@mail.missouri.edu>
2018-05-09 10:26:13 -0600

    268

---
+++ src/repo_2018/missingNumber_268/Solution.java
@@ -0,0 +1,37 @@
+package repo_2018.missingNumber_268;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+    public int missingNumber(int[] nums) {
+        Set<Integer> numSet = new HashSet<Integer>();
+        for (int num : nums) numSet.add(num);
+
+        int expectedNumCount = nums.length + 1;
+        for (int number = 0; number < expectedNumCount; number++) {
+            if (!numSet.contains(number)) {
+                return number;
+            }
+        }
+        return -1;
+    }
+
+    public int missingNumberXor(int[] nums) {
+        int missing = nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            missing ^= i ^ nums[i];
+        }
+        return missing;
+    }
+
+    /**
+     * We can compute the sum of nums in linear time, and by Gauss' formula, we can compute the sum of the first nn natural numbers in constant time. Therefore, the number that is missing is simply the result of Gauss' formula minus the sum of nums, as nums consists of the first nn natural numbers minus some number.
+     */
+    public int missingNumberGauss(int[] nums) {
+        int expectedSum = nums.length*(nums.length + 1)/2;
+        int actualSum = 0;
+        for (int num : nums) actualSum += num;
+        return expectedSum - actualSum;
+    }
+}
