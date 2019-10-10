bab9a0772a2eaec57453e4f0ab7977ddb88fd749
ruiqi.shi <rs79c@mail.missouri.edu>
2019-10-10 16:08:01 -0600

    q 27

--- src/repo_2019_2020/first400/array/q27_removeElement.java
+++ src/repo_2019_2020/first400/array/q27_removeElement.java
@@ -16,25 +16,35 @@ package repo_2019_2020.first400.array;
  * It doesn't matter what you leave beyond the returned length.
  * <p>
  * <p>
- * Algorithm
- * <p>
- * When nums[j]nums[j] equals to the given value, skip this element by incrementing jj. As long as nums[j] \neq valnums[j]
- * 
- * ​
- * =val, we copy nums[j]nums[j] to nums[i]nums[i] and increment both indexes at the same time. Repeat the process until jj reaches the end of the array and the new length is ii.
+ */
+
+/**
+ * * Algorithm
+ * * <p>
+ * * When nums[j]nums[j] equals to the given value, skip this element by incrementing jj. As long as nums[j] \neq valnums[j]
+ * * 
+ * * ​
+ * * =val, we copy nums[j]nums[j] to nums[i]nums[i] and increment both indexes at the same time. Repeat the process until jj reaches the end of the array and the new length is ii.
  */
 public class q27_removeElement {
     public int removeElement(int[] nums, int val) {
         int j = 0; //slow runner
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != val) {
-                nums[j] = nums [i];
+                nums[j] = nums[i];
                 j++;
             }
         }
         return j;
     }
 
+    /**
+     * Algorithm
+     * <p>
+     * When we encounter nums[i] = valnums[i]=val, we can swap the current element out with the last element and dispose the last one. This essentially reduces the array's size by 1.
+     * <p>
+     * Note that the last element that was swapped in could be the value you want to remove itself. But don't worry, in the next iteration we will still check this element.
+     */
     public int removeElement2(int[] nums, int val) {
         int i = 0;
         int n = nums.length;
