4364ca47bfae428741017d63f7153d79b5fb8803
ruiqi.shi <rs79c@mail.missouri.edu>
2019-11-06 09:23:25 -0700

    q4 modified

--- src/repo_2019_2020/first400/array/q4_medianOfTwoSortedArray.java
+++ src/repo_2019_2020/first400/array/q4_medianOfTwoSortedArray.java
@@ -2,16 +2,16 @@ package repo_2019_2020.first400.array;
 
 public class q4_medianOfTwoSortedArray {
     /**
-     *Analysis:
+     * Analysis:
      * In order to solve this question, we need to first understand what a median is. A median is the middle value of a dataset.
      * Since we have 2 seperately sorted array in this question, to find the middle value is somewhat complicated. However, keep in mind that we do not care about the actual value of the numbers, what we want is the middle point from the combination of 2 arrays. In other words, we are looking for the middle index of the 2 arrays. Thus approach like binary search could be employed.
      * Based on the fact that the 2 arrays are sorted seperatedly, we could try to get the submedian of the 2 arrays in each round. Than compare them.
-     *
+     * <p>
      * And the basic idea is that the left half of the array with a smaller submedian can never contains the common median.
      */
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         // Deal with invalid corner case.
-        if (nums1 == null || nums2 == null || nums1.length == 0 || nums2.length == 0) return 0.0;
+        if ((nums1 == null && nums2 == null) || (nums1.length == 0 && nums2.length == 0)) return 0.0;
 
         int m = nums1.length, n = nums2.length;
         int l = (m + n + 1) / 2; //left half of the combined median
@@ -47,6 +47,7 @@ public class q4_medianOfTwoSortedArray {
             return getKth(nums1, start1, nums2, start2 + k / 2, k - k / 2); //nums1 + nums2.right
         }
     }
+
     public static double findMedianSortedArraysNormal(int[] nums1, int[] nums2) {
         int i = 0;
         int j = 0;
@@ -55,25 +56,25 @@ public class q4_medianOfTwoSortedArray {
             if (nums1[i] < nums2[j]) {
                 nums3[i + j] = nums1[i];
                 i++;
-            }else{
+            } else {
                 nums3[i + j] = nums2[j];
                 j++;
             }
         }
 
-            while (i < nums1.length) {
-                nums3[i + j] = nums1[i];
-                i++;
-            }
+        while (i < nums1.length) {
+            nums3[i + j] = nums1[i];
+            i++;
+        }
 
-            while (j < nums2.length) {
-                nums3[i + j] = nums2[j];
-                j++;
-            }
+        while (j < nums2.length) {
+            nums3[i + j] = nums2[j];
+            j++;
+        }
 
 
         if ((nums3.length) % 2 == 0) {
-            return ((double)(nums3[nums3.length / 2] + nums3[nums3.length / 2 - 1])) / 2;
+            return ((double) (nums3[nums3.length / 2] + nums3[nums3.length / 2 - 1])) / 2;
         } else {
             return nums3[nums3.length / 2];
         }
@@ -81,9 +82,9 @@ public class q4_medianOfTwoSortedArray {
     }
 
     public static void main(String[] args) {
-        int[] t1 = new int[]{1,2,3};
-        int[] t2 = new int[]{2,3,4};
-        System.out.println(findMedianSortedArraysNormal(t1,t2));
+        int[] t1 = new int[]{1, 2, 3};
+        int[] t2 = new int[]{2, 3, 4};
+        System.out.println(findMedianSortedArraysNormal(t1, t2));
     }
 
 }
