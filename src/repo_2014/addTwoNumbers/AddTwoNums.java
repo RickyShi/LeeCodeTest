f6bca5dd87081473366bb6c7efbb35c17d2bbae9
ruiqi.shi <rs79c@mail.missouri.edu>
2019-11-14 16:00:08 -0700

    refine

--- src/repo_2014/addTwoNumbers/AddTwoNums.java
+++ src/repo_2014/addTwoNumbers/AddTwoNums.java
@@ -4,56 +4,39 @@ package repo_2014.addTwoNumbers;
  * You are given two linked lists representing two non-negative numbers. The
  * digits are stored in reverse order and each of their nodes contain a single
  * digit. Add the two numbers and return it as a linked list.
- * 
+ * <p>
  * Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
  * Output: 7 -> 0 -> 8
- * 
+ *
  * @author Ricky
- * 
  */
- class ListNode {
-	int val;
-	ListNode next;
+class ListNode {
+    int val;
+    ListNode next;
 
-	ListNode(int x) {
-		val = x;
-		next = null;
-	}
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
 }
 
 public class AddTwoNums {
-	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-		ListNode head = null;
-		ListNode tail = null;
-		ListNode node1 = l1;
-		ListNode node2 = l2;
-		// carry means 进位
-		int carry = 0;
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(-1);
+        ListNode tmp = head;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int val1 = l1 == null ? 0 : l1.val;
+            int val2 = l2 == null ? 0 : l2.val;
+            int sum = val1 + val2 + carry;
+            tmp.next = new ListNode(sum % 10);
+            carry = sum / 10;
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+            tmp = tmp.next;
+        }
+        if (carry > 0) tmp.next = new ListNode(carry);
+        return head.next;
 
-		while (node1 != null || node2 != null) {
-			int val1 = node1 == null ? 0 : node1.val;
-			int val2 = node2 == null ? 0 : node1.val;
-			int sum = val1 + val2 + carry;
-			carry = sum / 10;
-			ListNode newNode = node1 == null ? node2 : node1;
-			newNode.val = sum % 10;
-			if (head == null) {
-				head = newNode;
-				tail = newNode;
-			} else {
-				tail.next = newNode;
-				tail = tail.next;
-			}
-		}
-		// we still have carry on the last number
-		if (carry > 0) {
-			tail.next = new ListNode(carry);
-			tail = tail.next;
-		}
-
-		if (tail != null) {
-			tail.next=null;
-		}
-		return head;
-	}
+    }
 }
