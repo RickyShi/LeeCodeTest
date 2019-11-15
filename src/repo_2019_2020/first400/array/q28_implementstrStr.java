03562c82f54a335fa4c42d583bdacba81e213420
ruiqi.shi <rs79c@mail.missouri.edu>
2019-11-14 17:03:01 -0700

    28. Implement strStr()

---
+++ src/repo_2019_2020/first400/array/q28_implementstrStr.java
@@ -0,0 +1,12 @@
+package repo_2019_2020.first400.array;
+
+public class q28_implementstrStr {
+    public int strStr(String s, String t) {
+        if (t.isEmpty()) return 0; // edge case: "",""=>0  "a",""=>0
+        for (int i = 0; i <= s.length() - t.length(); i++) {
+            for (int j = 0; j < t.length() && s.charAt(i + j) == t.charAt(j); j++)
+                if (j == t.length() - 1) return i;
+        }
+        return -1;
+    }
+}
